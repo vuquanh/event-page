1c117849d3d7c3a19d4d363156bbe1f4d80a5bf6
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-05 17:31:50 -0700

    Fixed errors by installing Boostrap.

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react'
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-      <header>
-       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <Navbar.Brand href="/">Team 2 Event Page</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-narbar-nav">
-            <Nav className="ms-auto">
-            <Nav.Link href="/cart"><i class="fa-solid fa-cart-plus"></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i class="fa-regular fa-user"></i>Log In</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-       </Navbar>
-      </header>
-  );
-};
+    <header>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+     <Container>
+       <Navbar.Brand href="/">Team 2 Event Page</Navbar.Brand>
+       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+       <Navbar.Collapse id="basic-narbar-nav">
+         <Nav className="ms-auto">
+         <Nav.Link href="/cart"><i class="fa-solid fa-cart-plus"></i>Cart</Nav.Link>
+         <Nav.Link href="/login"><i class="fa-regular fa-user"></i>Log In</Nav.Link>
+         </Nav>
+       </Navbar.Collapse>
+     </Container>
+    </Navbar>
+   </header>
+  )
+}
 
-export default Header;
+export default Header
