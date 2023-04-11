f3f73459037d7141c89aee6499327035ac7a1c1f
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-11 09:38:13 -0700

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {  Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import EventScreen from "./screens/EventScreen";
-import Brand from "./components/brand";
+
 
 
 const App = () => {
