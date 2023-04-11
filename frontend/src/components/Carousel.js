f5d08675b993c5af219e22fe3651faf60785b0fb
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-10 18:43:22 -0700

    changed the color of footer

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -4,28 +4,22 @@ function CarouselFadeExample({ items }) {
   console.log("Carousel Items:", items);
 
   return (
+    <Carousel fade className="carousel-container">
+      {items.map((item) => (
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`/images/${item._id}.png`}
+            alt="First slide"
+            style={{borderRadius: "20px"}}
+          />
 
-      <Carousel fade>
-       
-        {items.map((item) => (
-         
-      
-         <Carousel.Item >
-            <img
-                className="d-block w-100"
-              src={`/images/${item._id}.png`}
-              alt="First slide"
-            />
-
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-            </Carousel.Item>
-         
-       ))}
-      </Carousel>
-
+          {/* <Carousel.Caption>
+            <h2 style={{color: "red"}}>{item.name}</h2>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 }
 
