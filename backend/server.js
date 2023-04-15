d432573026e3eea70a06ed9cc71b41e3815e5d90
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-15 12:58:55 -0700

    edited evnets.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const events = require ('./data/events')
+
+const app = express();
+app.get('/api/events', (req, res) => {
+    res.json(events)
+}) 
+
+app.get('/api/event/:id', (req, res) => {
+    const event = events.find(e => e._id === req.params.id)
+    res.json(event)
+})
+
+app.listen(5000, console.log ('server is running on port 5000'))
