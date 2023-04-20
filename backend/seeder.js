fa4ddefe8761b292fc03abcdc168b60cb7770a05
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-20 14:19:13 -0700

    Added process.argv in seeder.js

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import events from "./data/events.js";
+//import users sample data later
 import Event from "./models/eventModel.js";
+//import User model later
+
 import connectDB from "./config/db.js";
 
 dotenv.config(); //this command lets us have access to .env file
@@ -8,7 +11,7 @@ connectDB(); //this will connect to database and will print out the host if ther
 
 const importData = async () => {
   try {
-    //you need to delete existing data in MongoDB first before insering new ones.
+    //we are deleting existing data in MongoDB first before insering new ones.
     //when deleting data, you need to delete the child data first which is Event in this case.
     await Event.deleteMany();
     await User.deleteMany();
@@ -34,7 +37,7 @@ const destroyData = async () => {
   try {
     await Event.deleteMany();
     await User.deleteMany();
-    
+
     console.log("Data destroyed");
     process.exit();
   } catch (error) {
@@ -42,3 +45,12 @@ const destroyData = async () => {
     process.exit(1); //?? check later
   }
 };
+
+//argv stands for argument vector. process.argv is a coomand for node.js.
+// "data:destroy": "node backend/seeder.js -d" --- "node" is index 0,  "backend/seeder.js" is index 1,  "-d" is index 2
+// If there is an index 2 and the value at index 2 is equal to '-d', then the destroyData function will be executed. Otherwise, the importData function will be executed.
+if(process.argv[2] === '-d') { 
+    destroyData();
+} else {
+    importData();
+}
