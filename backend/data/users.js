d8325d8a8cbf2d5eca01867f2605948333c8f770
Imran S <55154759+ImranS19@users.noreply.github.com>
2023-04-26 16:45:30 -0400

    create encrypted password hash

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from bcryptjs
+
+const users = [{
+    name: 'Admin user',
+    email: 'info@example.org',
+    password: bcrypt.hashsync('123456', 10), 
+    isAdmin: true,
+},
+{
+    name: 'John Doe',
+    email: 'john@example.org',
+    password: bcrypt.hashsync('123456', 10),  
+
+},
+{
+    name: 'Jane Doe',
+    email: 'jane@example.org',
+    password: bcrypt.hashsync('123456', 10),     
+}
+]
+
+export default users
