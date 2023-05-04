90cb253d6ea3fbe87f94c17abf3ec6d621f2532a
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-05-04 11:20:42 -0700

    Added my tasks

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import { Col, Row } from "react-bootstrap";
-import axios from 'axios';
 import Event from "../components/Event";
 import Carousel from "../components/Carousel";
 
+//For reudx
+import { useSelector, useDispatch } from "react-redux";
+import { listEvents } from "../actions/eventActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+
 
 const HomeScreen = () => {
-  const[events, setEvents] = useState([])
+  const dispatch = useDispatch();
+  const eventList = useSelector((state) => state.eventList);
+  // console.log('state', eventList)
+  const { loading, error, events } = eventList;
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      const {data} = await axios.get('/api/events')
-      setEvents(data)
-    }
-    fetchEvents()
-  })
+    dispatch(listEvents());
+  }, [dispatch]);
   
   return(
     <>
-        <Carousel items = {events}/>
+    <Carousel items={events} />
 
-
-      <Row>
-        <Col xl={3}></Col>
-      </Row>
-      <h1> Events Near You </h1>
-      <Row>
-        {events.map((e, i) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={i}>
-            <Event event={e} />
-          </Col>
-        ))}
-      </Row>
+{/* <Row>
+  <Col xl={3}></Col>
+</Row> */}
+<h1> Events Near You </h1>
+{loading ? (
+  <Loader />
+) : error ? (
+  <Message variant="danger">{error}</Message>
+) : (
+  <Row>
+    {events.map((e, i) => (
+      <Col sm={12} md={6} lg={4} xl={3} key={i}>
+        <Event event={e} />
+      </Col>
+    ))}
+  </Row>
+)}
     </>
   );
 };
