d88ca4b438e09ed8faf2346ce27541f44ccf74c5
vuquanh <vuquanh@gmail.com>
2023-05-03 23:30:21 -0700

    Added eventActions.js

---
+++ frontend/src/actions/eventActions.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { EVENT_LIST_REQUEST, 
+    EVENT_LIST_FAIL,
+    EVENT_LIST_SUCCESS,
+    EVENT_DETAILS_FAIL,
+    EVENT_DETAILS_REQUEST,
+    EVENT_DETAILS_SUCCESS 
+} from "../constants/eventConstants"
+export const listEvent = () => async (dispatch) => {
+    try {
+        dispatch ({type: EVENT_LIST_REQUEST})
+        const {data} = await axios.get ('/api/events')
+        dispatch ({
+            type: EVENT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch ({
+            type: EVENT_LIST_FAIL,
+            payload: error.response && error.response.data.message 
+            ? error.response.data.message
+            : error.message
+        })
+
+    }
+}
+export const listEventDetails = (id) => async (dispatch) => {
+    try{
+      dispatch({type: EVENT_DETAILS_REQUEST})
+      const {data}= await axios.get(`/api/events/${id}`)
+      dispatch({
+        type: EVENT_DETAILS_SUCCESS,
+        payload: data
+      })
+  
+    } catch (error){
+      dispatch({
+        type: EVENT_DETAILS_FAIL,
+        payload: 
+          error.response && error.response.data.message 
+          ? error.response.data.message 
+          : error.message
+      })
+    }
+  }
