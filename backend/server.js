b7b297359fd07fe3b12a0ec23ebc9ec30f69f839
amandarazmus <amandarazmus@gmail.com>
2023-05-25 21:53:52 -0700

    Remove favicon

--- backend/server.js
+++ backend/server.js
@@ -35,5 +35,5 @@ res.send(process.env.PAYPAL_CLIENT_ID)
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT ||5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
